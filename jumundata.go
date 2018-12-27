d7f5083f3b5dac29afc4059d606e87fa1cb6a923
Vivek Roy <thezealousfool@gmail.com>
2018-12-28 01:56:00 +0530

    Html page

--- jumundata.go
+++ jumundata.go
@@ -314,7 +314,19 @@ func getSingleDelegZipHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func handleRoot(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprint(w, "Hello World")
+    fmt.Fprint(w, `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name="viewport" />
+            <title>JUMUN Data</title>
+        </head>
+        <body>
+            <h1>Hello World</h1>
+        </body>
+        </html>
+    `)
 }
 
 func main() {
